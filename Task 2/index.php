a9e4b7ecbf01bb9344b809a6e70ca04c8b6a433e
veloukarthik2022 <karthik.v@redblox.io>
2022-10-17 20:57:18 +0530

    switch case added task 2

--- Task 2/index.php
+++ Task 2/index.php
@@ -2,12 +2,13 @@
 
 
 
-abstract class Cardetail{
+abstract class Cardetail
+{
 
     protected $isBroken;
     protected $type;
 
-    public function __construct($type,$isBroken)
+    public function __construct($type, $isBroken)
     {
         $this->isBroken = $isBroken;
         $this->type = $type;
@@ -15,23 +16,36 @@ abstract class Cardetail{
 
     public function isBroken()
     {
-        echo $this->isBroken ? $this->type." is broken <br>" : $this->type." is not broken <br>";
+        switch ($this->type) {
+            case "Door":
+                echo $this->isBroken ? $this->type . " is broken <br>" : $this->type . " is not broken <br>";
+                break;
+            case "Tyre":
+                echo $this->isBroken ? $this->type . " is puncher <br>" : $this->type . " is not puncher <br>";
+                break;
+            case "Paint":
+                echo $this->isBroken ? $this->type." is scratched <br>" : $this->type." is not scratched <br>";
+                break;
+            default:
+                break;
+        }
     }
 }
 
-class Door extends Cardetail{
-
+class Door extends Cardetail
+{
 }
 
-class Tyre extends Cardetail{
-
+class Tyre extends Cardetail
+{
 }
 
-class Paint extends Cardetail{
-
+class Paint extends Cardetail
+{
 }
 
-class Car{
+class Car
+{
 
     protected $details;
 
@@ -42,14 +56,12 @@ class Car{
 
     public function getCarDetails()
     {
-        foreach($this->details as $details)
-        {
+        foreach ($this->details as $details) {
             $details->isBroken();
         }
-        
     }
 }
 
-$car = new Car([new Door("Door",true),new Tyre("Tyre",true),new Paint("Paint",true)]);
+$car = new Car([new Door("Door", true), new Tyre("Tyre", true), new Paint("Paint", true)]);
 
 $car->getCarDetails();
